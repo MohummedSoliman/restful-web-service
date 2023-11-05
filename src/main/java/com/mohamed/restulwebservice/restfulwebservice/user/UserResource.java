9dd6a179471a759ecb53f56aafbfa56bfefddb4b
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-11-05 14:22:26 +0200

    add controller to manage user requests

---
+++ src/main/java/com/mohamed/restulwebservice/restfulwebservice/user/UserResource.java
@@ -0,0 +1,28 @@
+package com.mohamed.restulwebservice.restfulwebservice.user;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserResource {
+
+	private UserDaoService service;
+	
+	public UserResource(UserDaoService service) {
+		this.service = service;
+	}
+	
+	@GetMapping("users")
+	public List<User> retrieveAllUsers(){
+		return service.findAll();
+	}
+	
+	@GetMapping("users/{id}")
+	public User retrieveUser(@PathVariable int id) {
+		User user = service.findById(id);
+		return user;
+	}
+}
