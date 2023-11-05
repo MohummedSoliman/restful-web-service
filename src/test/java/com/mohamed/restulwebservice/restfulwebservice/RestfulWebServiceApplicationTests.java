1dc5e142c46a54dac8d9cf042291001eb136e7d1
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-11-05 11:06:16 +0200

    initial commit

---
+++ src/test/java/com/mohamed/restulwebservice/restfulwebservice/RestfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohamed.restulwebservice.restfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
