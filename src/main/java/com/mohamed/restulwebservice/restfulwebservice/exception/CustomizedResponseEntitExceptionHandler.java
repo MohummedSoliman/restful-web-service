1a4e897744bb27c35988055f1544a37365874248
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-11-06 09:20:52 +0200

    add custom handle exception methods

--- src/main/java/com/mohamed/restulwebservice/restfulwebservice/exception/CustomizedResponseEntitExceptionHandler.java
+++ src/main/java/com/mohamed/restulwebservice/restfulwebservice/exception/CustomizedResponseEntitExceptionHandler.java
@@ -2,6 +2,8 @@ package com.mohamed.restulwebservice.restfulwebservice.exception;
 
 import java.time.LocalDateTime;
 
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,22 +13,23 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 import com.mohamed.restulwebservice.restfulwebservice.user.UserNotFoundException;
 
-
+@Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
-public class CustomizedResponseEntitExceptionHandler extends ResponseEntityExceptionHandler{
-	
+public class CustomizedResponseEntitExceptionHandler extends ResponseEntityExceptionHandler {
+
 	@ExceptionHandler(Exception.class)
-	public final ResponseEntity<ErrorDetails> handleAllException(Exception ex, WebRequest request){
-		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
-				ex.getMessage(), request.getDescription(false));
+	public final ResponseEntity<ErrorDetails> handleAllException(Exception ex, WebRequest request) {
+		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(),
+				request.getDescription(false));
 		return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
-	
+
 	@ExceptionHandler(UserNotFoundException.class)
-	public final ResponseEntity<ErrorDetails> handleUserNotFoundException(Exception ex, WebRequest request){
-	
-		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
-				ex.getMessage(), request.getDescription(false));
+	public final ResponseEntity<ErrorDetails> handleUserNotFoundException(Exception ex, WebRequest request) {
+
+		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(),
+				request.getDescription(false));
 		return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
 	}
+	
 }
